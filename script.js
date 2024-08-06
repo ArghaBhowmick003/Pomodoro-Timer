9192849d4155337bdf33acc5c2481c91e877f8d1
Argha Bhowmick <arghabhowmick003@gmail.com>
2024-08-06 13:14:28 +0530

    Few changes

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const startElement = document.getElementById("start");
+const stopElement = document.getElementById("stop");
+const resetElement = document.getElementById("restart");
+const timerElement = document.getElementById("timer");
+
+startElement.addEventListener("click");
